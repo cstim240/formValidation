721929b058ea2b68232d8b10c4de90a648fde0cb
Timothy Chico Supan <timsupann@gmail.com>
2024-03-25 21:18:53 -0700

    Add eventlistener for live feedback - currently nonfunctional for email

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
-const email = document.querySelector("emailInput");
+const form = document.querySelector(".form");
+const email = document.querySelector(".emailInput");
+const emailError = document.querySelector(".mailError");
+
+email.addEventListener("input", (event) => {
+    //each time the user types something, we check if the form fields are valid
+    if (email.validity.valid){
+        emailError.textContent = "";
+        emailError.className = "error"; //reset visual state of the message
+    } else {
+        showError();
+    }
+});
+
+//if the email is valid we let the form submit
+form.addEventListener("submit", (event) => {
+    if (!email.validity.valid){
+        showError();
+        event.preventDefault(); 
+        //prevent the form from being sent by cancelling the event
+    }
+});
+
+function showError(){
+    if (email.validity.valueMissing){
+        //if the field is empty, display the following error message
+        emailError.textContent = "You need to enter an email address!";
+    } else if (email.validity.typeMismatch){
+        //if the field doesn't contain an email address
+        emailError.textContent = "Entered value needs to be an email address";
+    } else if (email.validity.tooShort) {
+        email.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+    }
+
+    //set the styling appropriately
+    emailError.className = "error active";
+}
+/*const email = document.querySelector("emailInput");
 email.addEventListener("input", (event) => {
     if (email.validity.typeMismatch){
         email.setCustomValidity("I am expecting an email address!");
     } else {
         
     }
-});
+});*/
